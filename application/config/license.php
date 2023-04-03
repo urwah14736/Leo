5c68093237e44b4e5d02e70e2832c5989747fbcb
root <root@vmi340805.contaboserver.net>
2023-04-03 10:38:45 +0100

    first commit

---
+++ application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = '9PP3U9-NQ7PTF-5ZF2DW-aHQzVk1zUTQzN0p3N08rSTJGN1pZQzk4MXY5RVdHdEhtU09DcXF1a3ZzND0=';
+$config['app_ver'] = 0;
